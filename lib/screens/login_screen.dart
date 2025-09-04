1bfbea4404e579f28635ec9d39fcf41de5b50eee
Potato1Peeler <alexku05100@gmail.com>
2025-09-04 10:38:55 -0600

    feat(login): add email and password inputs

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -9,15 +9,66 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  bool _obscurePassword = true; // Al inicio la contraseña está oculta
   @override
   Widget build(BuildContext context) {
+    //Para obtener el tamaño de la pantalla del dispositivo
+    final Size size = MediaQuery.of(context).size;
+
     return Scaffold(
       //Evita nudge o cámaras frontales para móviles
       body: SafeArea(
+          child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 50),
         child: Column(
           children: [
-            Expanded(child: RiveAnimation.asset('assets/animated_login_character.riv'))
+            SizedBox(
+                width: size.width,
+                height: 200,
+                child:
+                    RiveAnimation.asset('assets/animated_login_character.riv')),
+            //Espacio entre el oso y el texto email
+            const SizedBox(
+              height: 10,
+            ),
+            //Campo de texto del email
+            TextField(
+              //Para que aparezza el @ en móviles
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                  hintText: "Email",
+                  prefixIcon: Icon(Icons.mail),
+                  border: OutlineInputBorder(
+                      //Esquinas redondeadas
+                      borderRadius: BorderRadius.circular(12))),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            //Campo de texto del email
+            TextField(
+              obscureText: _obscurePassword,
+              decoration: InputDecoration(
+                  hintText: "Password",
+                  prefixIcon: Icon(Icons.lock),
+                  suffixIcon: InkWell(
+                    onTap: () {
+                      setState(() {
+                        _obscurePassword = !_obscurePassword;
+                      });
+                    },
+                    child: Icon(
+                      _obscurePassword
+                          ? Icons.visibility
+                          : Icons.visibility_off,
+                    ),
+                  ),
+                  border: OutlineInputBorder(
+                      //Esquinas redondeadas
+                      borderRadius: BorderRadius.circular(12))),
+            )
           ],
+        ),
       )),
     );
   }
